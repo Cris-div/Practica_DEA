defc49e727cd732ef8632b356babfc12288ccb11
Cris-div <christian.unocc@tecsup.edu.pe>
2025-09-17 14:52:35 -0500

    aaa

--- vuelos/models.py
+++ vuelos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Vuelo(models.Model):
+    codigo=models.IntegerField()
+    origen=models.CharField(max_length=100)
+    destino=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.codigo
+
+class Aerolinea(models.Model):
+    nombre=models.CharField(max_length=100)
+    pais_origen=models.CharField(max_length=100)
+    vuelo=models.ForeignKey(Vuelo,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nombre
--- vuelos/views.py
+++ vuelos/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Vuelo,Aerolinea
 # Create your views here.
+
+def vuelo_list(request):
+    vuelos=Vuelo.objects.all()
+    return render(request, 'vuelo/vuelo_list.html',{'vuelos':vuelos})
